c52b817e47b9dfcf011088ac90f58c0572078003
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-10-04 15:06:27 -0300

    refactor: change to less than

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const lunchText = document.querySelector('#lunchText')
 const hours = new Date().getHours()
 
-if (hours >= 11 && hours <= 15) {
+console.log(hours)
+
+if (hours >= 11 && hours < 15) {
   lunchText.innerHTML = 'Sim'
 } else {
   lunchText.innerHTML = 'Não'
