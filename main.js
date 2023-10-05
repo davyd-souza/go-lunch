ae7aa956375e4506f35daddccc13e58dc3a6534f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-10-04 23:42:39 -0300

    feat: implement dinner page

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const lunchText = document.querySelector('#lunchText')
+const dinnerText = document.querySelector('#dinnerText')
+
 const hours = new Date().getHours()
 
-console.log(hours)
+if(lunchText) {
+  if (hours >= 11 && hours < 15) {
+    lunchText.innerHTML = 'Sim'
+  } else {
+    lunchText.innerHTML = 'Não'
+  }
+}
 
-if (hours >= 11 && hours < 15) {
-  lunchText.innerHTML = 'Sim'
-} else {
-  lunchText.innerHTML = 'Não'
+if (dinnerText) {
+  if (hours >= 17 && hours < 22) {
+    dinnerText.innerHTML = 'Sim'
+  } else {
+    dinnerText.innerHTML = 'Não'
+  }
 }
